211a246ae62246a439bd1a19172325ece6a6cbf6
Ryan Rowland <rrowland@twilio.com>
2017-09-13 12:55:24 -0700

    Initial commit

---
+++ test/AudioPlayer.ts
@@ -0,0 +1,11 @@
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+
+import AudioPlayer from '../src/AudioPlayer';
+
+// tslint:disable-next-line:only-arrow-functions
+describe('AudioPlayer', function() {
+  it('should load', () => {
+    const audioPlayer = new AudioPlayer();
+  });
+});
