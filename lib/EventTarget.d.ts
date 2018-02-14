e2a2631243d29a3a37a7b1b33bf09427d8ee4737
twilio-ci <rrowland+twilio-ci@twilio.com>
2018-02-14 23:11:41 +0000

    1.0.1-dev

--- lib/EventTarget.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-import { EventEmitter } from 'events';
-export default class EventTarget {
-    private _eventEmitter;
-    addEventListener(name: string, handler: Function): EventEmitter;
-    dispatchEvent(name: string, ...args: any[]): boolean;
-    removeEventListener(name: string, handler: Function): EventEmitter;
-}
